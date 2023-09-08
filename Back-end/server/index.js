8673b6ff6cb922c07f365957eabc9822af2749f1
IHEEB <132073063+IHEEBEU@users.noreply.github.com>
2023-09-08 20:43:30 +0100

    Update index.js

--- Back-end/server/index.js
+++ Back-end/server/index.js
@@ -3,7 +3,7 @@ const connectDatabase = require('../db/config');
 const userRoutes = require('../db/Routes/userRoutes');
 const productRoutes = require('../db/Routes/productRoutes.js'); // Check the path
 
-const PORT = 5000
+
 const app = express();
 const cors = require('cors');
 
@@ -15,8 +15,8 @@ connectDatabase(); // Call the database connection function
 app.use('/', userRoutes);
 app.use('/', productRoutes);
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+const PORT = 5001;
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
 });
-
 module.exports = app;
