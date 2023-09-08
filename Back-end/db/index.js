2e87b58e764ea71f72b180c135e71b8af6fe1835
IHEEB <132073063+IHEEBEU@users.noreply.github.com>
2023-09-08 20:40:34 +0100

    Update index.js

--- Back-end/db/index.js
+++ Back-end/db/index.js
@@ -6,37 +6,6 @@ const User = require('./Models/userModels.js');
   try {
     await connectDatabase();
   
-    const newUser = await User.create({
-      username: 'john_doe',
-      email: 'john@example.com',
-      password: 'hashed_password',
-      image: 'profile.jpg',
-      phoneNumber: '123-456-7890',
-      city: 'New York',
-    });
-
-    const newProduct = await Product.create({
-      name: 'Product Name',
-      description: 'This is the product description...',
-      price: 29.99,
-      image: 'product_image.jpg',
-      quantityAvailable: 100,
-    });
-
-    const newReview = {
-      user: newUser._id,
-      text: 'This product is amazing!',
-      rating: 5,
-    };
-
-    newProduct.reviews.push(newReview);
-    await newProduct.save();
-
-    newUser.reviewedProducts.push(newProduct._id);
-    await newUser.save();
-
-    console.log('User:', newUser);
-    console.log('Product:', newProduct);
   } catch (error) {
     console.error('Error:', error);
   }
